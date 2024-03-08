a058738091cc9cc23d7d086763bc738ea767c9dc
Jenne9 <95435999+Jenne9@users.noreply.github.com>
2024-03-08 18:15:44 +0100

    GA

---
+++ terraform/terraform.tf
@@ -0,0 +1,104 @@
+resource "random_pet" "sg" {}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
+resource "aws_volume_attachment" "ebs_attachment" {
+  device_name = "/dev/xvdf"  # Nom du périphérique sur l'instance (peut varier selon l'OS)
+  instance_id = aws_instance.opencti.id
+  volume_id   = aws_ebs_volume.ebs.id
+}
+
+resource "aws_ebs_volume" "ebs" {
+  availability_zone = aws_instance.opencti.availability_zone
+  size              = 20  # Taille du volume en GiB
+  type              = "gp2"  # Type de volume
+}
+
+resource "aws_instance" "opencti" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.2xlarge"
+  vpc_security_group_ids = [aws_security_group.app-sg.id]
+
+  tags = {
+    Name = "opencti"
+  }
+  
+}
+
+resource "aws_security_group" "app-sg" {
+  name = "${random_pet.sg.id}-sg"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 5672
+    to_port     = 5672
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 15672
+    to_port     = 15672
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 25
+    to_port     = 25
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "opencti-address" {
+  value = "${aws_instance.opencti.public_ip}:8080"
+}
