cba81ffd1dc5485f3a697fad194d74d9d72017c1
Jenne9 <95435999+Jenne9@users.noreply.github.com>
2024-03-09 14:56:47 +0100

    modif

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -31,7 +31,7 @@ resource "aws_ebs_volume" "ebs" {
 
 resource "aws_instance" "opencti" {
   ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
+  instance_type          = "t2.large"
   vpc_security_group_ids = [aws_security_group.app-sg.id]
 
   tags = {
